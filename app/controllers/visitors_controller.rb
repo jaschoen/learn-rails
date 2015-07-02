3d3e06dfc7c54be8a7b96425f84810d7898fb768
Justin Schoen <jaschoen@gmail.com>
2015-07-02 18:09:01 -0500

    front-end framework

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,10 +2,6 @@ class VisitorsController < ApplicationController
 
   def new
     @owner = Owner.new
-	render 'visitors/new', :layout => true
-	# render 'visitors/new', :layout => 'special' would use the 'special' layout
-	flash[:notice] = 'Welcome!'
-    flash.now[:alert] = "It's a thinking computer"
   end
 
 end
